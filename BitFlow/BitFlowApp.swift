0f74aceecc85a20900f60e93ae78fb56b9792ffb
shaahzaib <110594965+shaahzaib@users.noreply.github.com>
2025-06-18 02:38:24 +0500

    Initial Commit

---
+++ BitFlow/BitFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  BitFlowApp.swift
+//  BitFlow
+//
+//  Created by Macbook Pro on 18/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct BitFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BitFlow/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BitFlow
+//
+//  Created by Macbook Pro on 18/06/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
