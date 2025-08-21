20489a79121d1e21b0cac7c37a4f526b0fd6db51
shaahzaib <110594965+shaahzaib@users.noreply.github.com>
2025-08-21 15:54:26 +0500

    coin details view created and implemented successfully.

---
+++ BitFlow/CoinDetails/View/ChartView.swift
@@ -0,0 +1,18 @@
+//
+//  ChartView.swift
+//  BitFlow
+//
+//  Created by Macbook Pro on 20/08/2025.
+//
+
+import SwiftUI
+
+struct ChartView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChartView()
+}
